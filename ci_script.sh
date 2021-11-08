8b12eac38e3708e26e7e505025331bb7dbeec87f
HybridDog <ovvv@web.de>
2021-11-08 17:29:14 +0100

    remove an apt package

--- ci_script.sh
+++ ci_script.sh
@@ -6,7 +6,7 @@ download_schismtracker_and_depends() {
 	git clone --depth=1 https://github.com/schismtracker/schismtracker.git
 	# Install dependencies with apt
 	sudo apt-get install build-essential automake autoconf autoconf-archive    \
-                libx11-dev libxext-dev libxv-dev libxxf86misc-dev     \
+                libx11-dev libxext-dev libxv-dev     \
                 libxxf86vm-dev libsdl1.2-dev libasound2-dev mercurial \
 									libtool
 }
